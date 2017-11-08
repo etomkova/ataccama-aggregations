79fdfb1d79aaef4aba63d3b02aed932a46922b2c
Tomáš Čechal <tomas.cechal@firma.seznam.cz>
2017-11-08 12:47:57 +0100

    Generalized DataSet implementations tests.

--- src/test/java/com/github/tcechal/ataccama/aggregations/DataSetTest.java
+++ src/test/java/com/github/tcechal/ataccama/aggregations/DataSetTest.java
@@ -1,22 +1,59 @@
-package com.github.tcechal.ataccama.aggregations.dataset;
+package com.github.tcechal.ataccama.aggregations;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-import com.github.tcechal.ataccama.aggregations.DataSet;
+import com.github.tcechal.ataccama.aggregations.dataset.LazyDataSet;
+import com.github.tcechal.ataccama.aggregations.dataset.MemoryDataSet;
 import com.github.tcechal.ataccama.aggregations.lib.Ints;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 
-public class MemoryDataSetTest {
+@RunWith(Parameterized.class)
+public class DataSetTest {
+
+    private final Class implementationClass;
+
+    /**
+     * A constructor supporting parametrized tests.
+     */
+    public DataSetTest(Class implementationClass) {
+
+        this.implementationClass = implementationClass;
+    }
+
+    /**
+     * Parameters for the parametrized test runner, a list of DataSet implementations to be tested.
+     */
+    @Parameterized.Parameters
+    public static Collection<Object[]> implementations() {
+
+        Object[][] data = {{ MemoryDataSet.class }, { LazyDataSet.class }};
+        return Arrays.asList(data);
+    }
+
+    /**
+     * Helper method that calls "create" on the supplied class implementing the DataSet interface.
+     */
+    @SuppressWarnings("unchecked")
+    private <T> DataSet<T> createGeneric(Iterable<T> input) throws Exception {
+
+        Method method = implementationClass.getMethod("create", Iterable.class);
+
+        return (DataSet<T>) method.invoke(null, input);
+    }
 
     @Test
     public void createFromIterable() throws Exception {
 
         final List<String> input = Arrays.asList("cat", "dog");
-        final DataSet<String> dataset = MemoryDataSet.create(input);
+        final DataSet<String> dataset = createGeneric(input);
 
         Assert.assertEquals(input, dataset.collect());
     }
@@ -25,7 +62,7 @@ public class MemoryDataSetTest {
     public void createFromEmptyIterable() throws Exception {
 
         final List<String> input = Collections.emptyList();
-        final DataSet<String> dataset = MemoryDataSet.create(input);
+        final DataSet<String> dataset = createGeneric(input);
 
         Assert.assertEquals(input, dataset.collect());
     }
@@ -36,7 +73,7 @@ public class MemoryDataSetTest {
         final List<String> input = Arrays.asList("1", "2");
         final List<Integer> output = Arrays.asList(1, 2);
 
-        final DataSet<String> inputDataSet = MemoryDataSet.create(input);
+        final DataSet<String> inputDataSet = createGeneric(input);
         final DataSet<Integer> outputDataSet = inputDataSet.transform(Integer::parseInt);
 
         Assert.assertEquals(output, outputDataSet.collect());
@@ -48,7 +85,7 @@ public class MemoryDataSetTest {
         final List<String> input = Collections.emptyList();
         final List<Integer> output = Collections.emptyList();
 
-        final DataSet<String> inputDataSet = MemoryDataSet.create(input);
+        final DataSet<String> inputDataSet = createGeneric(input);
         final DataSet<Integer> outputDataSet = inputDataSet.transform(Integer::parseInt);
 
         Assert.assertEquals(output, outputDataSet.collect());
@@ -59,7 +96,7 @@ public class MemoryDataSetTest {
 
         final List<String> input = Arrays.asList("1", "crash");
 
-        final DataSet<String> inputDataSet = MemoryDataSet.create(input);
+        final DataSet<String> inputDataSet = createGeneric(input);
         final DataSet<Integer> outputDataSet = inputDataSet.transform(Integer::parseInt);
 
         outputDataSet.collect();
@@ -69,7 +106,7 @@ public class MemoryDataSetTest {
     public void simpleAggregation() throws Exception {
 
         final List<Integer> input = Arrays.asList(1, 2);
-        final DataSet<Integer> inputDataSet = MemoryDataSet.create(input);
+        final DataSet<Integer> inputDataSet = createGeneric(input);
 
         Assert.assertEquals(Integer.valueOf(2), inputDataSet.count());
         Assert.assertEquals(Integer.valueOf(2), inputDataSet.aggregate(Ints::max));
@@ -79,7 +116,7 @@ public class MemoryDataSetTest {
     public void emptyAggregation() throws Exception {
 
         final List<Integer> input = Collections.emptyList();
-        final DataSet<Integer> inputDataSet = MemoryDataSet.create(input);
+        final DataSet<Integer> inputDataSet = createGeneric(input);
 
         Assert.assertNull(inputDataSet.aggregate(Ints::max));
     }
@@ -88,8 +125,7 @@ public class MemoryDataSetTest {
     public void chainedTransformations() throws Exception {
 
         final List<Integer> input = Arrays.asList(1, 2);
-        final DataSet<Integer> dataset = MemoryDataSet
-                .create(input)
+        final DataSet<Integer> dataset = createGeneric(input)
                 .transform(x -> x + 1)
                 .transform(x -> x + 1);
 
@@ -100,7 +136,7 @@ public class MemoryDataSetTest {
     public void forkedTransformations() throws Exception {
 
         final List<Integer> input = Arrays.asList(1, 2);
-        final DataSet<Integer> inputDataSet = MemoryDataSet.create(input);
+        final DataSet<Integer> inputDataSet = createGeneric(input);
         final DataSet<Integer> left = inputDataSet.transform(x -> x + 1);
         final DataSet<Integer> right = inputDataSet.transform(x -> x - 1);
 
@@ -113,8 +149,7 @@ public class MemoryDataSetTest {
 
         final List<String> input = Arrays.asList("1", "2");
 
-        final Integer result = MemoryDataSet
-                .create(input)
+        final Integer result = createGeneric(input)
                 .transform(Integer::parseInt)
                 .transform(x -> -x)
                 .aggregate(Ints::max);
